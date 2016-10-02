ce1a35823ec5af5107c641589c96538d423365f2
David Zambrano <davazamb10@outlook.com>
2016-10-02 19:53:12 -0400

    Finik

---
+++ src/MovieCore/Data/Migrations/20161002210110_Initia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieCore.Data.Migrations
+{
+    public partial class Initia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
